60e3c7a003a383acade7a69f5f845fd9863ea0b0
alexMarCar <68387828+alexMarCar@users.noreply.github.com>
2020-07-18 12:59:21 +0200

    Add files via upload

---
+++ WGCNAandLouvainNets.R
@@ -0,0 +1,21 @@
+
+#We created WGCNA network as simple as we show here. 
+#By using the function below from CoExpNets package, wecan create.
+library(CoExpNets)
+library(WGCNA)
+netWGCNA = getDownstreamNetwork(tissue="WGCNAfiltCorrected",
+                                 n.iterations=20,
+                                 net.type = "signed",
+                                 debug=F,
+                                 expr.data=AMPPDdata,
+                                 job.path=".")
+
+# We created the Louvain network as we show here
+# In this case, for creating the network, we first found communities over the graph made from expression datasets.
+# Then, we could generate a network object as we can see below
+library(igraph)
+communitiesLouvain = cluster_louvain(graphAMP.PD)
+netLouvain = NULL
+netLouvain$moduleColors = WGCNA::labels2colors(communitiesLouvain$membership) # We add a color for each different community found
+names(netLouvain$moduleColors) = communitiesLouvain$names # We name them
+saveRDS(object = netLOu20M, file = "LouvainNet20M.rds") # We save the net
